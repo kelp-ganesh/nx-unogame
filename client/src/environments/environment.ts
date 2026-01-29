a59bd363c4804f4ae5937a48a13432286dabb605
Ganesh Bhoyar <ganeshbhoyar@FVT089M.local>
2026-01-29 12:50:56 +0530

    feat:added docker file in client

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  // apiUrl: '/api',
-  // socketUrl:"https://qd89949c-3002.inc1.devtunnels.ms/"
-  apiUrl: 'http://localhost:3000/api',
-  socketUrl: 'http://localhost:3002',
+  apiUrl: 'https://qd89949c-3000.inc1.devtunnels.ms/api',
+  socketUrl:"https://qd89949c-3002.inc1.devtunnels.ms"
+  // apiUrl: 'http://localhost:3000/api',
+  // socketUrl: 'http://localhost:3002',
 };
